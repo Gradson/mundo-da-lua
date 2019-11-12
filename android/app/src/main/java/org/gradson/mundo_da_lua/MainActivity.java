5059193e1777545ca1ae6b0f58be89d12691731b
Gradson <gmesquita@atech.com.br>
2019-11-12 15:25:14 -0300

    initial

---
+++ android/app/src/main/java/org/gradson/mundo_da_lua/MainActivity.java
@@ -0,0 +1,13 @@
+package org.gradson.mundo_da_lua;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mundo_da_lua/Home.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
